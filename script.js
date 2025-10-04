de12c9db1a9bc132add9449c1a4df38698d5c6a9
Manarte-Dev <220400922+Manarte-Dev@users.noreply.github.com>
2025-10-04 14:43:19 -0300

    previsão do tempo

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ overlay.addEventListener('click', () => {
 // BOTÃO TOPO
 const btnTopo = document.getElementById('btnTopo');
 window.addEventListener('scroll', () => {
-  if(window.scrollY > 300){
+  if (window.scrollY > 300) {
     btnTopo.classList.add('show');
   } else {
     btnTopo.classList.remove('show');
@@ -25,3 +25,36 @@ window.addEventListener('scroll', () => {
 btnTopo.addEventListener('click', () => {
   window.scrollTo({ top: 0, behavior: 'smooth' });
 });
+
+// Previsão do tempo 
+
+const weatherWidget = document.querySelector('#weatherWidget');
+
+// Capturar localização
+
+if ('geolocation' in navigator) {
+  navigator.geolocation.getCurrentPosition(async (pos) =>{
+  const lat = pos.coords.latitude;
+  const lon = pos.coords.longitude;
+
+  // chave api
+
+  const apiKey = 'bd2edff2e859a2079483d141048b1fa2';
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&lang=pt_br&appid=${apiKey}`
+
+  try {
+    const res = await fetch(url);
+    const data = await res.json();
+    const temp = Math.round(data.main.temp);
+    const desc = data.weather[0].description;
+
+    weatherWidget.textContent = `🌤️ ${temp} °C - ${desc}`;
+  }    catch (err) {
+    weatherWidget.textContent = `Não foi possivel obter dados`
+  }
+}, () => {
+  weatherWidget.textContent = `Ative sua localização`;
+});
+} else {
+  weatherWidget.textContent = `Geolocalização não suportada`;
+}
